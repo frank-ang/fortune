cf8fc3ca588d0730968810159148be2ddf44ae2c
Frank Ang <frankang@amazon.com>
2020-01-03 15:34:13 +0800

    App security group update for private NLB

--- config/configure.sh
+++ config/configure.sh
@@ -41,10 +41,11 @@ echo "export DB_HOST="`aws ssm get-parameter --name $DB_ENDPOINT_PARAMETER_NAME 
 
 # extract Fargate config
 echo "export API_ENDPOINT="`aws cloudformation describe-stacks --stack-name $CONTAINER_STACK_NAME \
-			| jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "PublicLoadBalancerDNSName") | .OutputValue'` >> $PROPERTIES_FILE
+			| jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "LoadBalancerDNSName") | .OutputValue'` >> $PROPERTIES_FILE
 echo "export ECR_REPOSITORY_URI="`aws ecr describe-repositories --repository-names $ECR_REPOSITORY_NAME \
 			| jq -r '.repositories[0].repositoryUri'`  >> $PROPERTIES_FILE
-echo "export COMMIT_HASH="`git log -1 --pretty=format:'%H' | cut -c 1-7` >> $PROPERTIES_FILE
+COMMIT_HASH=`git log -1 --pretty=format:'%H' | cut -c 1-7`
+echo "export COMMIT_HASH=$COMMIT_HASH" >> $PROPERTIES_FILE
 echo "export CONTAINER_IMAGE_URL="`if [ -n "$ECR_REPOSITORY_URI" ] && [ -n "$COMMIT_HASH" ]; \
 			then echo $ECR_REPOSITORY_URI:$COMMIT_HASH; else echo nginx; fi` >> $PROPERTIES_FILE
 
